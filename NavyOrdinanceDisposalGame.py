f93ba9b1d968d74680fb4dc092b70e4395fbbf3c
andrew-song18 <38329555+andrew-song18@users.noreply.github.com>
2018-05-13 20:08:11 -0700

    Add files via upload

--- NavyOrdinanceDisposalGame.py
+++ NavyOrdinanceDisposalGame.py
@@ -1,25 +1,27 @@
-#player setup over spring break through turtle
-#week 1: set up of turtle and user control, imported libraries
+#player setup over spring break through turtle, not pygame
+#week 1: set up of superclass of sprite for turtle imported libraries
 #week 2: tijuana immersion, didn't really work on code as much as I would have wanted to
-#week 3: create boundaries detection and draw borders, initial listening of keyboard
-#week 4: defined collision if turtle hits boundaries. Created github repository. Created "mine" class
+#week 3: create boundaries detection and draw borders, listening of keyboard
+#week 4: defined collision if turtle hits boundaries. Created github repository. 
 #week 4 cont: having hard time with user detecting collision with "mine" class.
-#week 5: Recreated "mine" class to be more functionable
+#week 5: Recreated "mine" class to be more functionable 
 #week 6: game class created, mine and user collison now works
 #week 7: created shell class, figured out code was broken because failed to intilialize shells to emerge from mine after explosion
-#week 8: Blitting and using images to replace simple surrogate polygons, emphasizing aesthetic gameplay.
-#week 9: scoreboard, TBD must blit turtle,landmines,background. Customize
+#week 8: Imported images to replace the sprites, emphasizing aesthetic gameplay.
+#week 9: scoreboard of # of landmines, 
+#week 10 (project deadline weekend): commented on all code and organized github repository with more commits
 
 #import libraries
-
 import random
 import time
 import turtle 
 
+#setting up background with ocean animation still image
 bgTurtle = turtle
 screenTurtle = bgTurtle.getscreen()
 screenTurtle.bgpic("ocean.gif")
 
+#turtle characteristics
 turtle.fd(0)
 turtle.speed(0)
 turtle.bgcolor('blue')
@@ -27,6 +29,7 @@ turtle.ht()
 turtle.setundobuffer(1)
 turtle.tracer(0)
 
+#create superclass of sprites as all subclasses share similiar characteristics
 class Sprite(turtle.Turtle):
 	def __init__(self, spriteshape, color, startx, starty):
 		turtle.Turtle.__init__(self, shape = spriteshape)
@@ -40,7 +43,7 @@ class Sprite(turtle.Turtle):
 	def move(self):
 		self.fd(self.speed)
 		
-		#Boundary detection
+		# border walls before game class
 		if self.xcor() > 290:
 			self.setx(290)
 			self.rt(60)
@@ -56,7 +59,9 @@ class Sprite(turtle.Turtle):
 		if self.ycor() < -290:
 			self.sety(-290)
 			self.rt(60)
-			
+		
+		#border walls
+
 	def is_collision(self, other):
 		if (self.xcor() >= (other.xcor() - 20)) and \
 		(self.xcor() <= (other.xcor() + 20)) and \
@@ -66,6 +71,7 @@ class Sprite(turtle.Turtle):
 		else:
 			return False
 				
+#player class with Battleship image replacing standard turtle shape
 class Player(Sprite):
 	def __init__(self, spriteshape, color, startx, starty):
 		Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -74,7 +80,7 @@ class Player(Sprite):
 		s.addshape(image)
 		self.shape(image)
 		s.bgcolor("black")
-		self.shapesize(stretch_wid=1.8, stretch_len=1.2, outline=None)
+		self.shapesize(stretch_wid=2.8, stretch_len=2.2, outline=None)
 		self.speed = 5
 		self.lives = 3
 
@@ -86,19 +92,31 @@ class Player(Sprite):
 		
 	def decelerate(self):
 		self.speed -= 1
-		
+
+#landmine class uses shapsize that is -50,50 because it needs to be smaller than the user's battleship to "detonate"
+#just called landmine because it's easier than calling class underwater mine
 class Landmine(Sprite):
 	def __init__(self, spriteshape, color, startx, starty):
 		Sprite.__init__(self, spriteshape, color, startx, starty)
+		s = turtle.Screen()
+		image = "underwater.gif"
+		s.addshape(image)
+		self.shape(image)
+		self.shapesize(-50,50)
 		self.speed = 0
 		self.setheading(random.randint(0,360))
 
-class Torpedo(Sprite):
+class Submarine(Sprite):
 	def __init__(self, spriteshape, color, startx, starty):
 		Sprite.__init__(self, spriteshape, color, startx, starty)
+		s = turtle.Screen()
+		image = "torpedo.gif"
+		s.addshape(image)
+		self.shape(image)
 		self.speed = 2
 		self.setheading(random.randint(0,360))
 
+#shells are the orange particles that appear after user destroys an underwater mine
 class Shell(Sprite):
 	def __init__(self, spriteshape, color, startx, starty):
 		Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -119,6 +137,8 @@ class Shell(Sprite):
 		if self.frame > 15:
 			self.frame = 0
 			self.goto(-1000, -1000)
+
+#class to define borders 
 
 class Game():
 	def __init__(self):
@@ -144,7 +164,7 @@ class Game():
 		
 	def show_status(self):
 		self.pen.undo()
-		msg = "Landmines: %s" %(self.score+19)
+		msg = "Landmines: %s" %(self.score+9)
 		self.pen.penup()
 		self.pen.goto(-50, 310)
 		self.pen.write(msg, font=("Times New Roman", 18, "normal"))
@@ -155,15 +175,17 @@ game.draw_border()
 
 game.show_status()
 
-player = Player("triangle", "black", -160, 0)
+#player class needs square shape to trigger collision detection with landmine. Image does not detect collision, but shape of turtle.
+player = Player("square", "black", -160, 0)
 
+#write the number of landmines on plane
 Landmines =[]
 for i in range(10):
 	Landmines.append(Landmine("square", "blue", -120, 0))
-
-Torpedoes =[]
-for i in range(3):
-	Torpedoes.append(Torpedo("triangle", "red", -120, 0))
+#write the number of submarines on the game plane
+Submarines =[]
+for i in range(2):
+	Submarines.append(Submarine("triangle", "red", -120, 0))
 
 shells = []
 for i in range(45):
@@ -179,7 +201,7 @@ turtle.listen()
 # image1 = pygame.image.load('ocean.jpg')
 # screen.blit(image1, [200, 200])
 
-#Main game loop
+#main game loop
 while True:
 	turtle.update()
 	time.sleep(0.02)
@@ -199,14 +221,14 @@ while True:
 			for shell in shells:
 				shell.explode(player.xcor(), player.ycor())
 
-	for Torpedo in Torpedoes:
-		Torpedo.move()
+	for Submarine in Submarines:
+		Submarine.move()
 		
 		#Check for a trigger with player
-		if player.is_collision(Torpedo):
+		if player.is_collision(Submarine):
 			x = random.randint(-250, 250)
 			y = random.randint(-250, 250)
-			Torpedo.goto(x, y)
+			Submarine.goto(x, y)
 			game.score -=10
 			game.show_status()
 			for shell in shells:
@@ -215,8 +237,7 @@ while True:
 	for shell in shells:
 		shell.move() 
 
-
-
+#game is infinite
 
     
 
